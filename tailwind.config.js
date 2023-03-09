1ebeae2e26c4b08e6892b8d566b37495e3270626
Zenza235 <paoloatienza235@gmail.com>
2023-03-09 14:04:16 -0500

    added custom palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: [],
   theme: {
+    colors:{
+      purple: {
+        100: '#18122B',
+        200: '#393053',
+        300: '#443C68',
+        400: '#635985',
+      }
+    },
     extend: {},
   },
   plugins: [],
