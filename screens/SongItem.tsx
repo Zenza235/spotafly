100c16b8becb5c9bb9ab5f801a40d5c4ffd02532
SkyanSam <samuelwiggins15@gmail.com>
2023-03-17 14:18:03 -0400

    attempt to fix dynamic requiring

--- screens/SongItem.tsx
+++ screens/SongItem.tsx
@@ -11,14 +11,18 @@ const SongItem: React.FC<{
         <View style={{backgroundColor: "#FFFFFF", alignItems: "flex-start"}}>
             <Text>{song}</Text>
             <Text>{artist}</Text>
-            <CoverImage filename={image}/>
+            <CoverImage filename={image} source={require("../assets/covers/" + image)}/>
         </View>
     );
 };
+// DONT USE DYNAMIC REQUIRE..
+// this implementation may not even work
+// we should probably use IDs and then make a List of Node Requires so we require all images when app is run in beggining.
 
 // TODO: fix tailwind className issue
 const CoverImage: React.FC<{
     filename: string
+    source : NodeRequire
 }> = (filename) => {
     return (
         <View>
